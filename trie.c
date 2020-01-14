36ead3388504df91106e11af8dc289e7d9a61477
Jordan <jordantangy@gmail.com>
2020-01-14 15:30:22 +0200

    implementing add function

--- trie.c
+++ trie.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "trie.h"
+#include<ctype.h>
 #define ascii_max 122
+
+
 
 
 
@@ -9,66 +12,83 @@ void print(const Node* root) {
 
 }
 
-void add(Node* root, char* word){
-
-	
-	int index = NUM_LETTERS-(ascii_max-*word)-1;
-
-	if(isNull(root,index) == 0){
-		Node* temp = malloc(sizeof(Node));
-		temp->letter=*word;
-		temp->count = 0;
-		*(temp->children) = NULL;
-		root->children[index] = temp;
+boolean check_if_exist(Node* root, int index){
+	if(root->children[index] != NULL){
+		return TRUE;
 	}
-	else{
-		*root++;
-		*word++;
-		add(root,word);
+	else {
+		return FALSE;
+	}
+}
+
+
+void add(Node* root){
+
+	char c = getchar();
+	while (c = getchar())
+	{
+		if (c == EOF)
+		{
+			break;
+		}
+		int ascii_value = tolower(c);		
+		int index = NUM_LETTERS-(ascii_max-ascii_value)-1;
+
+		if(c == ' '){
+			c = c--;
+			int ascii_value = tolower(c);		
+			int index = NUM_LETTERS-(ascii_max-ascii_value)-1;
+			(root->children[index])->count = (root->children[index])->count++;
+			c++;
+			continue;
+		}
+		while(c == (root->children[index])->letter && check_if_exist(root,index) == TRUE ){
+			Node* ptr= root->children[index];
+			root = ptr;
+			int flag = 0;
+			for (size_t i = 0; i < NUM_LETTERS; i++)
+			{
+				if(ptr->children[i] == NULL){
+					flag = 1;
+				}
+			}
+			if (flag == 1){
+				break;
+			}
+			else
+			{
+				c++;
+			}
+	
+		}
+		Node* temp = malloc(sizeof(Node));
+		temp->letter=c;
+		temp->count = 0;
+		for (size_t i = 0; i < NUM_LETTERS; i++)
+			{
+				temp->children[i] = NULL;
+				
+			}
+		root->children[index] = temp;
+		root = temp;
+
+		free(temp);
 	}
 		
-	
-	
-
 }
 
-boolean isNull(Node* ptr, int index){
-	if(ptr->children[index] == NULL){
-		return 0;
-	}
-	else{
-		return 1;
-	}	
-}
-
-char* getword(char w[]){
-
-    int counter = 0;
-	char* c = w;
-    while (*c != ''){
-        counter++;
-        c++;
-    }
-	return c;
-
-}
 
 int main() {
-	
-	
-	Node *root = malloc(sizeof(Node));
-	root->letter = NULL;
-	root->count = 0;
 
-	char word[30] = {0};
-    char *wordP = word;
-    while(1){
-        scanf("%c", wordP);
-        if (*wordP == ' '){
-            *wordP = '\n';
-            break;
-        }
-		add(root,wordP);
-        wordP++;
-    }
+	Node* root = malloc(sizeof(Node));
+	root->letter = 0;
+	root->count = 0;
+	*(root->children) = NULL;
+	
+		
+		add(root);
+		
+	
+        
+    
 }
