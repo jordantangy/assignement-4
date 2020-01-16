a672450cf37268126ba90a425a49c5eb12d6f153
Jordan <jordantangy@gmail.com>
2020-01-17 00:50:03 +0200

    updated version

--- trie.c
+++ trie.c
@@ -5,15 +5,8 @@
 #define ascii_max 122
 
 
-
-
-
-void print(const Node* root) {
-
-}
-
 boolean check_if_exist(Node* root, int index){
-	if(root->children[index] != NULL){
+	if(root->children[index] != NULL) {
 		return TRUE;
 	}
 	else {
@@ -21,74 +14,88 @@ boolean check_if_exist(Node* root, int index){
 	}
 }
 
+char if_space(){
+    char c;
+    int b = 0;
+    int *t;
+    int a = 0;
+	while (1)
+	{
+        c = getchar();
+        a = c;
+         if (a == 32)
+		    {
+            t = &b;
+            a = *t;
+            return a;
+            }
+        b = c;
+   }  
+}
+
+Node* Search_letter(Node* root, char c,int index){
+        while (root->children[index]->letter = c)
+        {
+            root = root->children[index];
+        }
+        return root; 
+    }
+
 
 void add(Node* root){
 
-	char c = getchar();
-	while (c = getchar())
-	{
-		if (c == EOF)
-		{
-			break;
-		}
-		int ascii_value = tolower(c);		
-		int index = NUM_LETTERS-(ascii_max-ascii_value)-1;
+        char d = if_space();
 
-		if(c == ' '){
-			c = c--;
-			int ascii_value = tolower(c);		
-			int index = NUM_LETTERS-(ascii_max-ascii_value)-1;
-			(root->children[index])->count = (root->children[index])->count++;
-			c++;
-			continue;
-		}
-		while(c == (root->children[index])->letter && check_if_exist(root,index) == TRUE ){
-			Node* ptr= root->children[index];
-			root = ptr;
-			int flag = 0;
-			for (size_t i = 0; i < NUM_LETTERS; i++)
-			{
-				if(ptr->children[i] == NULL){
-					flag = 1;
-				}
-			}
-			if (flag == 1){
-				break;
-			}
-			else
-			{
-				c++;
-			}
-	
-		}
+         int ascii_value = 0; 
+        int index = 0; 
+
+         char c;
+        while (c != EOF){
+            c = getchar();
+          
+            ascii_value = tolower(c);		
+		    index = NUM_LETTERS-(ascii_max-ascii_value)-1;
+            if(check_if_exist(root,index) == TRUE){
+              root =  Search_letter(root,c,index);
+        }
 		Node* temp = malloc(sizeof(Node));
 		temp->letter=c;
 		temp->count = 0;
 		for (size_t i = 0; i < NUM_LETTERS; i++)
 			{
 				temp->children[i] = NULL;
-				
 			}
-		root->children[index] = temp;
-		root = temp;
+		root->children[index] = temp;	
+        if(c == ' '){
+        ascii_value = tolower(d);		
+		index = NUM_LETTERS-(ascii_max-ascii_value)-1;
+        (root->children[index])->count = (root->children[index]->count)+1;
+        continue;
+       }
 
 		free(temp);
-	}
-		
-}
 
+	}
+
+    
+
+       
+
+}
 
 int main() {
 
 	Node* root = malloc(sizeof(Node));
 	root->letter = 0;
 	root->count = 0;
-	*(root->children) = NULL;
-	
-		
-		add(root);
-		
-	
-        
+	for (size_t i = 0; i < NUM_LETTERS; i++)
+	{
+		root->children[i] =  NULL;
+	}
+
     
+
+    add(root);
+
+        
 }
